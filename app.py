cbef350857e35a98c5f241e1d5d11a0b50cda664
Sufi1512 <ksufiyan38@gmai.com>
2024-03-03 02:31:26 +0530

    final App

--- app.py
+++ app.py
@@ -10,6 +10,33 @@ import requests
 
 app = Flask(__name__)
 
+#gemini
+from flask import Flask, render_template, request, jsonify
+import google.generativeai as genai
+
+app = Flask(__name__)
+
+genai.configure(api_key="AIzaSyCttke6ob3XoMomeyWhyW_BECf_b3_33S8")
+
+generation_config = {
+    "temperature": 0.9,
+    "top_p": 1,
+    "top_k": 1,
+    "max_output_tokens": 2048,
+}
+
+safety_settings = [
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+]
+
+model = genai.GenerativeModel(
+    model_name="gemini-1.0-pro",
+    generation_config=generation_config,
+    safety_settings=safety_settings,
+)
 # Ensure 'uploads' directory exists
 uploads_dir = os.path.join(app.instance_path, 'uploads')
 os.makedirs(uploads_dir, exist_ok=True)
@@ -69,6 +96,18 @@ def detect():
         return jsonify({"error": "No file uploaded"})
     return render_template('image.html')
 
+@app.route('/gemini', methods=['GET', 'POST'])
+def gemini():
+    return render_template('gem.html')
+@app.route('/api/send_message', methods=['POST'])
+def send_message():
+    user_input = request.json['user_input']
+
+    convo = model.start_chat(history=[])
+    convo.send_message(user_input)
+    genai_response = convo.last.text
+
+    return jsonify({'genai_response': genai_response})
 
 if __name__ == '__main__':
     app.run(debug=True)
